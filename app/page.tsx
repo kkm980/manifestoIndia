844d3d2398b5f4ec00a029d56f8560a05f8276b3
krishna9802 <krishna.k@inventives.ai>
2023-08-05 14:57:14 +0530

    scroll-bar stable

--- app/page.tsx
+++ app/page.tsx
@@ -56,7 +56,7 @@ export default function Home() {
         />
       </div>
 
-      {/* <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
+      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
@@ -125,7 +125,7 @@ export default function Home() {
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
         </a>
-      </div> */}
+      </div>
 
       <div style={{ marginBottom: '4rem', textAlign: 'center' }}>
         <h4 style={{ marginBottom: 16 }}>{count}</h4>
@@ -150,6 +150,52 @@ export default function Home() {
         <button onClick={() => dispatch(reset())}>reset</button>
       </div>
 
+
+      <div style={{ marginBottom: '4rem', textAlign: 'center' }}>
+        <h4 style={{ marginBottom: 16 }}>{count}</h4>
+        <button onClick={() => dispatch(increment())}>increment</button>
+        <button
+          onClick={() => dispatch(decrement())}
+          style={{ marginInline: 16 }}
+        >
+          decrement
+        </button>
+        <button onClick={() => dispatch(reset())}>reset</button>
+      </div>
+      <div style={{ marginBottom: '4rem', textAlign: 'center' }}>
+        <h4 style={{ marginBottom: 16 }}>{count}</h4>
+        <button onClick={() => dispatch(increment())}>increment</button>
+        <button
+          onClick={() => dispatch(decrement())}
+          style={{ marginInline: 16 }}
+        >
+          decrement
+        </button>
+        <button onClick={() => dispatch(reset())}>reset</button>
+      </div>
+
+      <div style={{ marginBottom: '4rem', textAlign: 'center' }}>
+        <h4 style={{ marginBottom: 16 }}>{count}</h4>
+        <button onClick={() => dispatch(increment())}>increment</button>
+        <button
+          onClick={() => dispatch(decrement())}
+          style={{ marginInline: 16 }}
+        >
+          decrement
+        </button>
+        <button onClick={() => dispatch(reset())}>reset</button>
+      </div>
+      <div style={{ marginBottom: '4rem', textAlign: 'center' }}>
+        <h4 style={{ marginBottom: 16 }}>{count}</h4>
+        <button onClick={() => dispatch(increment())}>increment</button>
+        <button
+          onClick={() => dispatch(decrement())}
+          style={{ marginInline: 16 }}
+        >
+          decrement
+        </button>
+        <button onClick={() => dispatch(reset())}>reset</button>
+      </div>
       {error ? (
         <p>Oh no, there was an error</p>
       ) : isLoading || isFetching ? (
--- components/Home/Header.tsx
+++ components/Home/Header.tsx
@@ -1,25 +1,31 @@
-"use client"
+"use client";
 
-import {ProfileContainer} from "@/components/Home/ProfileContainer"
-import { ModeToggle } from "./ToggleMode"
-import {NavBar} from "@/components/Home/NavBar"
+import { ProfileContainer } from "@/components/Home/ProfileContainer";
+import { ModeToggle } from "./ToggleMode";
+import { NavBar } from "@/components/Home/NavBar";
 import { useTheme } from "next-themes";
-export default function Header(){
-    const { theme } = useTheme();
-   return (
-     <div className="flex justify-end items-center nav-bar w-full h-[50px] bg-saffron fixed top-0 z-50">
-       <NavBar />
-       <div
-         className={`h-[30px] w-[70px] flex justify-center items-center rounded rounded-full p-1`}
-         style={{ backgroundColor: theme === "dark" ? "black" : "white" }}
-       >
-        <div className=" ease-in duration-1000 p-0 m-0" style={{ order: theme === "dark" ? "1" : "2" }}>
+export default function Header() {
+  const { theme } = useTheme();
+  return (
+    <div className="flex justify-end items-center nav-bar w-full h-[50px] bg-saffron fixed top-0 z-50">
+      <NavBar />
+      <div
+        className={`h-[30px] w-[70px] flex justify-center items-center rounded rounded-full p-1`}
+        style={{ backgroundColor: theme === "dark" ? "black" : "white" }}
+      >
+        <div
+          className=" ease-in duration-1000 p-0 m-0"
+          style={{ order: theme === "dark" ? "1" : "2" }}
+        >
           <ProfileContainer />
         </div>
-        <div className=" ease-in duration-1000 p-0 m-0" style={{ order: theme === "dark" ? "2" : "1" }}>
+        <div
+          className=" ease-in duration-1000 p-0 m-0"
+          style={{ order: theme === "dark" ? "2" : "1" }}
+        >
           <ModeToggle />
         </div>
-       </div>
-     </div>
-   );
+      </div>
+    </div>
+  );
 }
