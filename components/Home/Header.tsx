68aae766861e91294f68479f34dedcffdecded98
krishna9802 <krishna.k@inventives.ai>
2023-08-05 14:26:11 +0530

    stable navbar

--- components/Home/Header.tsx
+++ components/Home/Header.tsx
@@ -10,7 +10,7 @@ export default function Header(){
      <div className="flex justify-end items-center nav-bar w-full h-[50px] bg-saffron fixed top-0 z-50">
        <NavBar />
        <div
-         className={`flex justify-center items-center rounded rounded-full p-1 pt-1.5 px-2`}
+         className={`h-[30px] w-[70px] flex justify-center items-center rounded rounded-full p-1`}
          style={{ backgroundColor: theme === "dark" ? "black" : "white" }}
        >
         <div className=" ease-in duration-1000 p-0 m-0" style={{ order: theme === "dark" ? "1" : "2" }}>
