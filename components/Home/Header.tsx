8d1d180f55705c29b97d8e66177fafc60750fefc
krishna9802 <krishna.k@inventives.ai>
2023-08-05 14:10:53 +0530

    stable navbar

--- components/Home/Header.tsx
+++ components/Home/Header.tsx
@@ -6,14 +6,20 @@ import {NavBar} from "@/components/Home/NavBar"
 import { useTheme } from "next-themes";
 export default function Header(){
     const { theme } = useTheme();
-   return(
-    <div className="flex justify-end items-center nav-bar w-full h-[50px] bg-saffron fixed top-0 z-50">
-        <NavBar/>
-        <div className={`flex justify-center items-center rounded rounded-full p-0.5 pr-1`}
-        style={{backgroundColor:theme==='dark'?'black':'white'}}>
-        <ProfileContainer/>
-        <ModeToggle/></div>
-        
-    </div>
-   )
+   return (
+     <div className="flex justify-end items-center nav-bar w-full h-[50px] bg-saffron fixed top-0 z-50">
+       <NavBar />
+       <div
+         className={`flex justify-center items-center rounded rounded-full p-1 pt-1.5 px-2`}
+         style={{ backgroundColor: theme === "dark" ? "black" : "white" }}
+       >
+        <div className=" ease-in duration-1000 p-0 m-0" style={{ order: theme === "dark" ? "1" : "2" }}>
+          <ProfileContainer />
+        </div>
+        <div className=" ease-in duration-1000 p-0 m-0" style={{ order: theme === "dark" ? "2" : "1" }}>
+          <ModeToggle />
+        </div>
+       </div>
+     </div>
+   );
 }
